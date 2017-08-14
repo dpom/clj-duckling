cfec5a7c38f72e51aac71967ba1efb11b8e4d877
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-08-14 15:46:22 +0300

    adds a scratch file for repl

--- src/duckling/core.clj
+++ src/duckling/core.clj
@@ -541,17 +541,3 @@
         (log/errorf e "duckling error err=%s" (pr-str err))
         []))))
 
-;; for tests
-;; (def corpus (make-corpus "ro" ["finance"
-;;                                "time"
-;;                                "communication"
-;;                                "temperature"
-;;                                "numbers"
-;;                                "measure"]))
-;; (def rules (make-rules "ro"  ["finance"
-;;                               "time"
-;;                               "communication"
-;;                               "temperature"
-;;                               "numbers"
-;;                               "measure"]))
-;; (learn/train-classifiers corpus rules learn/extract-route-features)
---
+++ src/duckling/scratch.clj
@@ -0,0 +1,18 @@
+;; use for repl
+
+(def corpus (make-corpus "ro" ["finance"
+                               "time"
+                               "communication"
+                               "temperature"
+                               "numbers"
+                               "measure"]))
+
+(def rules (make-rules "ro"  ["finance"
+                              "time"
+                              "communication"
+                              "temperature"
+                              "numbers"
+                              "measure"]))
+
+(learn/train-classifiers corpus rules learn/extract-route-features)
+
