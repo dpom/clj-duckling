5c3a2b5c93b877d22723a83aa07839cfe5e6b603
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-08-30 07:09:58 +0300

    tries to fix the budget rules

--- resources/languages/ro/rules/budget.clj
+++ resources/languages/ro/rules/budget.clj
@@ -2,13 +2,15 @@
 "max budget"
 [#"(?i)sub|max(\.|im)?" (dim :amount-of-money)]
  {:dim :budget
-  :value (:value %2)
-  :unit (:unit %2)
-  :level "max"}
-;; (assoc %2 :dim :budget :level "max")
+  :value {:value (:value %2)
+          :unit (:unit %2)
+          :level "max"}}
 
 "min budget"
 [#"(?i)peste|min(\.|im)?" (dim :amount-of-money)]
-(assoc %2 :dim :budget :level "min")
+ {:dim :budget
+  :value {:value (:value %2)
+          :unit (:unit %2)
+          :level "min"}}
 )
 
