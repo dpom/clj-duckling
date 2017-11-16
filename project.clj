7bdc5522fecc00f8d3c45afa20cdecd4bcd9b46f
Dan Pomohaci <dan.pomohaci@gmail.com>
2017-11-16 11:44:10 +0200

    update libs

--- project.clj
+++ project.clj
@@ -1,22 +1,23 @@
-(defproject dpom/clj-duckling "0.6.2-dev02"
+(defproject dpom/clj-duckling "0.7-dev01"
   :description "A Clojure library that parses text into structured data"
   :license {:url "https://github.com/wit-ai/duckling"
             :comments "see LICENSE"}
   :url "https://dpom.github.io/clj-duckling/"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
                  [environ "1.1.0"]
-                 [clj-time "0.13.0"]
+                 [clj-time "0.14.2"]
                  [integrant "0.6.1"]
                  [duct/logger "0.2.1"]
-                 [duct/logger.timbre "0.4.1"]
-                 [prismatic/plumbing "0.5.4"]]
-  ;; :pedantic? :abort
+                 [duct/logger.timbre "0.4.1" :exclusions [com.taoensso/encore com.taoensso/truss]]
+                 [com.taoensso/nippy "2.13.0"] 
+                 [prismatic/plumbing "0.5.5"]]
+  :pedantic? :warning
   :plugins [[s3-wagon-private "1.1.2" :exclusions [commons-logging commons-codec]]
             [lein-ancient "0.6.10" :exclusions [commons-logging org.clojure/clojure]]
             [jonase/eastwood "0.2.4"]
             [lein-kibit "0.1.6-beta2" :exclusions [org.clojure/clojure]]
-            [lein-cljfmt "0.5.6" :exclusions [org.clojure/clojure]]
+            [lein-cljfmt "0.5.6" :exclusions [org.clojure/clojure rewrite-clj]]
             [lein-environ "1.1.0"]
             [lein-codox "0.10.3" :exclusions [org.clojure/clojure]]]
   :repl-options {:init-ns user}
@@ -27,7 +28,7 @@
                    :test-paths ["src"]
                    :dependencies [[integrant/repl "0.2.0"]
                                   [org.clojure/tools.trace "0.7.9"]
-                                  [cheshire "5.7.1"]]}
+                                  [cheshire "5.8.0"]]}
              :uberjar {:aot [clj-duckling.core]}}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
